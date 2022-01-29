93575e34bd738875c02b4eaf511d15c806eda7b0
ahmed_basiony <61539608+besioo@users.noreply.github.com>
2022-01-29 17:33:18 +0200

    Add files via upload

---
+++ h1_reports.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from termcolor import *
+
+output_file = open("reports.csv", "a")
+
+headers = {
+  'Accept': 'application/json'
+}
+
+for id in range(1357200,1500000):
+  r = requests.get(
+      f"https://api.hackerone.com/v1/hackers/reports/{id}",
+      auth=('ahmed_basiony', 'uoSOwZoaJbOPyOVs9cLZ4f6ZOoF9/hUN64GJMDHfl74='),
+      headers = headers,
+      timeout=10
+  )
+
+  report = json.loads(r.text)
+  try:
+     report['data']
+  except:
+     cprint(f"[-] {str(id)} Failed!", 'red')
+     continue
+  else:
+     report_id = report['data']['id']
+     report_title = report['data']['attributes']['title']
+     report_state = report['data']['attributes']['state']
+     link = f"https://hackerone.com/reports/{report_id}"
+     output_file.write(f"{id}, {link}, {report_title}, {report_state}\n")
+     cprint(f"[+] {str(id)} Success!", 'green')
+
+output_file.close()
+
